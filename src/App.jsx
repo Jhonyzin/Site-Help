f38a70eafb3d99c0f1eb4d92d17ee694c6bb6439
João <estudosjoao205@gmail.com>
2025-11-01 16:08:27 -0300

    arquivosiniciais

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import './App.css'
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
+import Login from "./pages/Login.jsx";
+import Teste from "./pages/Teste.jsx";
 
-function App() {
-  return (
-    <>
-    <p>Olá, mundao</p>
-    </>
-  )
+export default function App(){
+    return(
+        <Router>
+            <div style={{padding: 20}}>
+
+                <nav>
+                    <Link to="/login">Login</Link>
+                    <Link to="/teste">Teste</Link>
+                </nav>
+
+                <Routes>
+                    <Route path="/" element={<h1>Página Inicial</h1>} />
+                    <Route path="/login" element={<Login/>}/>
+                    <Route path="/teste" element={<Teste/>}/>
+                </Routes>
+            </div>
+        </Router>
+    )
 }
-
-export default App
